db9b2f00e5801363bb48988e6bc5a576d573ede3
Victor I. Afolabi <javafolabi@gmail.com>
2023-01-25 00:56:01 +0100

    Add docs to Data object

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,18 @@ use polars::prelude::*;
 
 pub mod data;
 
+/// Abstraction for spliting dataframe into ndarray.
 pub struct Data {
+  /// Column names.
   pub names: Vec<String>,
+  /// Data as ndarray.
   pub data: Array2<f64>,
 }
 
 impl Data {
+  /// Create a new Data split from a dataframe.
+  ///
+  /// Converts the dataframe into an ndarray.
   pub fn new(df: &DataFrame) -> Self {
     let names = df.get_column_names_owned();
     let data = df.to_ndarray::<Float64Type>().unwrap();
