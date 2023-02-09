1a1f5050c4c227f70d081412e4369d7ee7044631
Victor I. Afolabi <javafolabi@gmail.com>
2023-02-09 16:43:18 +0100

    Check if Result::is_ok() before unwraping

--- tests/utils.rs
+++ tests/utils.rs
@@ -10,19 +10,27 @@ use std::path::Path;
 // Load data into `DataFrame`.
 #[allow(dead_code)]
 pub fn load_df() -> DataFrame {
-  load_data(Some(Path::new("data/iris.csv"))).unwrap()
+  let df = load_data(Some(Path::new("data/iris.csv")));
+  assert!(df.is_ok());
+  df.unwrap()
 }
 
 #[allow(dead_code)]
 pub fn get_features_df() -> DataFrame {
   let df = load_df();
   let column_names = &df.get_column_names();
-  df.select(&column_names[0..4]).unwrap()
+
+  let features = df.select(&column_names[0..4]);
+  assert!(features.is_ok());
+  features.unwrap()
 }
 
 #[allow(dead_code)]
 pub fn get_target_df() -> DataFrame {
   let df = load_df();
   let column_names = &df.get_column_names();
-  df.select(&[column_names[4]]).unwrap()
+
+  let target = df.select(&[column_names[4]]);
+  assert!(target.is_ok());
+  target.unwrap()
 }
