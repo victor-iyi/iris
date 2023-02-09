d49541bf68dddf646cfa8978f048951918a4d1a4
Victor I. Afolabi <javafolabi@gmail.com>
2023-02-09 16:44:16 +0100

    Pre-process dataframe into ndarray

---
+++ tests/process.rs
@@ -0,0 +1,21 @@
+use iris::process::pre_process;
+mod utils;
+
+#[test]
+fn test_pre_process() {
+  // Load dataframe.
+  let df = utils::load_df();
+
+  // Pre-process dataframe.
+  let processed = pre_process(&df);
+  assert!(processed.is_ok());
+
+  let (data, feature_names, target_values) = processed.unwrap();
+
+  assert_eq!(data.shape(), [150, 5]);
+  assert_eq!(
+    feature_names,
+    ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+  );
+  assert_eq!(target_values, ["setosa", "versicolor", "verginica"]);
+}
